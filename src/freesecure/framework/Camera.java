2e08a6d2ab2beed7284d54c6eca8a696550962fb
ldanster <lebohangdanster@gmail.com>
2014-09-09 14:31:06 +0200

    Adding files for the Async calls

--- src/freesecure/framework/Camera.java
+++ src/freesecure/framework/Camera.java
@@ -1,7 +1,12 @@
 package freesecure.framework;
 
+import org.json.JSONArray;
+
+import freesecure.framework.interfaces.IAsyncDataProccessor;
+import freesecure.framework.utils.AsyncRequest;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Camera extends Activity {
 
@@ -9,10 +14,25 @@ public class Camera extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cameras);
-        
-        
-	
+        loadCameras();
     }
     
-	
+	private void loadCameras() {
+		
+		IAsyncDataProccessor dataproccessor = new IAsyncDataProccessor() {
+			
+			@Override
+			public void processAsyncDataError(Exception exception) {
+				Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG);
+			}
+			
+			@Override
+			public void processAsyncData(JSONArray jsonArray) {
+				Toast.makeText(getApplicationContext(), "Got some JSON back", Toast.LENGTH_LONG);
+			}
+		};
+		
+		AsyncRequest asyncRequest = new AsyncRequest(dataproccessor);
+		asyncRequest.execute("camera");
+	}
 }
--- src/freesecure/framework/utils/AsyncRequest.java
+++ src/freesecure/framework/utils/AsyncRequest.java
@@ -9,14 +9,14 @@ import android.os.AsyncTask;
 public class AsyncRequest extends AsyncTask<String, String, JSONArray> {
 
 	IAsyncDataProccessor processor;
+	
 	public AsyncRequest(IAsyncDataProccessor processor) {
 		this.processor = processor;
 	}
 	
 	@Override
 	protected JSONArray doInBackground(String... params) {
-		// TODO Auto-generated method stub
-		return null;
+		return new FreeSecureHttpRequest(params[0]).requestJson();
 	}
 	
 	@Override
